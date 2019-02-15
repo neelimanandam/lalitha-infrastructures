0b6567ab08ce54306c929f4901a706de5aedfa0c
Neelima <neelimanandam@gmail.com>
2019-02-14 20:07:11 -0500

    about us, our projects, contact us, footer

---
+++ components/Dashboard.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <v-carousel>
+      <v-carousel-item key="1">
+        <v-img src="/images/image0.jpg" />
+      </v-carousel-item>
+      <v-carousel-item key="2">
+        <v-img src="/images/image1.jpg" />
+      </v-carousel-item>
+      <v-carousel-item key="3">
+        <v-img src="/images/image2.jpg" />
+      </v-carousel-item>
+      <v-carousel-item key="4">
+        <v-img src="/images/image3.jpg" />
+      </v-carousel-item>
+    </v-carousel>
+    <v-spacer />
+    <v-container>
+      <v-card>
+        <v-card-text>
+          <h1 class="text-xs-center">
+            About Us
+          </h1>
+          <br>
+          <br>
+          <p>The founders of the company have a decade of specialized experience in development and construction of residential flats across Andhra Pradesh and Hyderabad. The company has grown from strength to strength, having successfully completed 20 prestigious projects of open residential land and residential apartments within Hyderabad and Andhra Pradesh.</p>
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card-text>
+        <h1 class="text-xs-center">
+          Our Projects
+        </h1>
+      </v-card-text>
+
+      <v-card>
+        <v-layout row="2">
+          <v-flex xs6>
+            <v-card-media src="/images/nstowers.jpg" />
+          </v-flex>
+
+          <v-flex xs6>
+            <v-card-text>
+              <h3>The founders of the company have a decade of specialized experience in development and construction of residential flats across Andhra Pradesh and Hyderabad. The company has grown from strength to strength, having successfully completed 20 prestigious projects of open residential land and residential apartments within Hyderabad and Andhra Pradesh.</h3>
+            </v-card-text>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card>
+        <v-card-text>
+          <h1 class="text-xs-center">
+            Our Team
+          </h1>
+          <br>
+          <br>
+          <p>The founders of the company have a decade of specialized experience in development and construction of residential flats across Andhra Pradesh and Hyderabad. The company has grown from strength to strength, having successfully completed 20 prestigious projects of open residential land and residential apartments within Hyderabad and Andhra Pradesh.</p>
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card-text>
+        <h1 class="text-xs-center">
+          Contact Us
+        </h1>
+      </v-card-text>
+
+      <v-card color="primary">
+        <v-layout row="2">
+          <v-flex xs6>
+            <v-card-text class="text-xs-center">
+              <v-icon>room</v-icon>
+              Srivijaya lakshmi vastralayam,<br>
+              Main Road<br>
+              Vuyyuru<br>
+              <v-icon>phone</v-icon>
+              7799416832.
+            </v-card-text>
+          </v-flex>
+
+          <v-flex xs6>
+            <v-card-text>
+              <h3>The founders of the company have a decade of specialized experience in development and construction of residential flats across Andhra Pradesh and Hyderabad. The company has grown from strength to strength, having successfully completed 20 prestigious projects of open residential land and residential apartments within Hyderabad and Andhra Pradesh.</h3>
+            </v-card-text>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
+    
+    <v-footer>
+      <v-card>
+        <v-card-title class="tead">
+          Get connected with us social networks!
+          <v-spacer />
+          <v-icon size="24px">
+            facebook
+          </v-icon>
+        </v-card-title>
+      </v-card>
+    </v-footer>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {}
+  }
+}
+</script>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,143 +1,16 @@
 <template>
-  <v-content>
-    <v-toolbar >
-      <v-toolbar-side-icon>
-        <v-icon>location_city</v-icon>
-      </v-toolbar-side-icon>
-
-      <v-toolbar-title>
-        Lalitha Infrastructures
-      </v-toolbar-title>
-      <v-spacer />
-      <v-toolbar-items>
-        <v-btn depressed small color="transperent">
-          Home
-        </v-btn>
-        <v-btn depressed small>
-          About Us
-        </v-btn>
-        <v-btn depressed small>
-          Projects
-        </v-btn>
-        <v-btn depressed small>
-          Flat Availability
-        </v-btn>
-        <v-btn depressed small>
-          Gallary
-        </v-btn>
-        <v-btn depressed small>
-          Contact us
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
-    <v-card>
-      <v-card-media :src="image">
-        <v-container fluid grid-list-lg>
-          <v-layout row wrap>
-            <v-flex>
-              <v-card color="blue-grey darken-2" class="white--text">
-                <v-card-title primary-title>
-                  <div>
-                    <div class="headline">
-                      Unlimited music now
-                    </div>
-                    <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
-                  </div>
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn flat >
-                    Listen now
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          
-
-            <v-flex xs12>
-              <v-card color="cyan darken-2" class="white--text">
-                <v-layout>
-                  <v-flex xs5>
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-                      height="125px"
-                      contain
-                    />
-                  </v-flex>
-                  <v-flex xs7>
-                    <v-card-title primary-title>
-                      <div>
-                        <div class="headline">
-                          Supermodel
-                        </div>
-                        <div>Foster the People</div>
-                        <div>(2014)</div>
-                      </div>
-                    </v-card-title>
-                  </v-flex>
-                </v-layout>
-                <v-divider light />
-                <v-card-actions class="pa-3">
-                  Rate this album
-                  <v-spacer />
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-
-            <v-flex xs12>
-              <v-card color="purple" class="white--text">
-                <v-layout row>
-                  <v-flex xs7>
-                    <v-card-title primary-title>
-                      <div>
-                        <div class="headline">
-                          Halycon Days
-                        </div>
-                        <div>Ellie Goulding</div>
-                        <div>(2013)</div>
-                      </div>
-                    </v-card-title>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-                      height="125px"
-                      contain
-                    />
-                  </v-flex>
-                </v-layout>
-                <v-divider light />
-                <v-card-actions class="pa-3">
-                  Rate this album
-                  <v-spacer />
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-media>
-    </v-card>
-  </v-content>
+  <div>
+    <dashboard />
+  </div>
 </template>
 
-
-
 <script>
+import Dashboard from '~/components/Dashboard'
+
 export default {
-  data() {
-    return {
-      image: require('@/assets/images/building-construction-ts-500.jpg')
-    }
-  }
+  components: {
+    Dashboard
+  },
+  layout: 'default'
 }
 </script>
