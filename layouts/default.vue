0f4a69b3da1b7fa3e53b65dd67bd3dd7d515cd21
Neelima <neelimanandam@gmail.com>
2019-02-14 20:07:11 -0500

    about us, our projects, contact us, footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,26 @@
 <template>
   <v-app>
     <v-content>
-      <v-container fluid fill-height>
-        <nuxt />>
-      </v-container>
+      <v-toolbar>
+        <v-toolbar-side-icon />
+        <v-toolbar-title>Lalitha Infrastructures</v-toolbar-title>
+        <v-spacer />
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>
+            Home
+          </v-btn>
+          <v-btn flat>
+            About Us
+          </v-btn>
+          <v-btn flat>
+            Projects
+          </v-btn>
+          <v-btn flat>
+            Contact Us
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <nuxt />
     </v-content>
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@ import Login from '~/components/Login'
 export default {
   components: {
     Login
-  }
+  },
+  layout: 'noNavigation'
 }
 </script>
