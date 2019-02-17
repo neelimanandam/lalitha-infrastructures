84cb8cdb642a241e3f72c95b159ced665c44e5aa
Neelima <neelimanandam@gmail.com>
2019-02-16 19:25:08 -0500

    Google Maps Integration

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ module.exports = {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules|dist)/
         })
       }
     }
