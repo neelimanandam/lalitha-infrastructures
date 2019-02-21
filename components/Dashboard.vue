e6619dab407a7acddd1e79e64005affb6726ee10
Neelima <neelimanandam@gmail.com>
2019-02-21 15:13:39 -0500

    Created grid list

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -5,13 +5,7 @@
         <v-carousel height="100%">
           <v-carousel-item v-for="image in images" :key="image.uid">
             <v-img style="height:100%" :src="image.source">
-              <v-layout
-                row
-                fill-height
-                justify-start
-                align-end
-                class="title font-weight-light"
-              >
+              <v-layout row fill-height justify-start align-end class="title font-weight-light">
                 <v-flex xs-12>
                   <h1 style="padding:100px">
                     {{ image.text }}
@@ -50,11 +44,23 @@
           </v-flex>
 
           <v-flex md6 xs12>
-            <v-container v-bind="{ [`grid-list-${size}`]: true }" fluid>
-              <v-layout row wrap>
-                <v-flex v-for="n in 9" :key="n" xs4 />
-              </v-layout>
-            </v-container>
+            <v-card>
+              <v-container v-bind="{ [`grid-list-${size}`]: true }" fluid>
+                <v-layout row wrap>
+                  <v-flex v-for="n in tabledata" :key="n.id" xs4>
+                    <v-card-text>
+                      {{ n.heading }}
+                      <v-spacer />
+                      {{ n.message1 }}
+                      <v-spacer />
+                      {{ n.subheading }}
+                      <v-spacer />
+                      {{ n.message2 }}
+                    </v-card-text>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
           </v-flex>
         </v-layout>
       </v-card>
@@ -97,6 +103,92 @@ export default {
           source: '/images/image3.jpg',
           uid: 3,
           text: '2 & 3 BHK Luxury Apartments @Vuyyuru'
+        }
+      ],
+      tabledata: [
+        {
+          heading: ' STRUCTURE:',
+          message1:
+            ' RCC framed structure with "Ultratech Cement" and "Simhadri TMT Steel.',
+          subheading: 'SUPERSTRUCTURE:',
+          message2:
+            'Red clay bricks with outside 9 inches walls andonside 4.5 inches walls.',
+          id: 1
+        },
+        {
+          heading: 'KITCHEN:',
+          message1:
+            'Granate cooking platform with SS sink and Ceramic tiles upto 2 inches height.',
+          subheading: 'CUPBOARDS:',
+          message2: 'High quality MDF Laminated Cupboards.',
+          id: 2
+        },
+        {
+          heading: ' MAIN DOORS:',
+          message1:
+            ' Designed teak wood frames and teak wood panel shutter with polish using brass/SS Fixtures.',
+          subheading: 'INTERNAL DOORS:',
+          message2: 'Teak wood frames with good quality moulded shutters.',
+          id: 3
+        },
+        {
+          heading: 'TOILETS:',
+          message1:
+            '7 inches Ceramic tiles for walls and non-skid Ceramic tiles on floor.',
+          subheading: 'SANITARY:',
+          message2: 'PVC sanitary lines of Astral/Finolex make.',
+          id: 4
+        },
+        {
+          heading: ' LIFTS:',
+          message1: ' 6 Passengers lift of standard make will be provided.',
+          subheading: 'GENERATOR:',
+          message2:
+            'Generator will be provided to Lift, Motor, Common lighting etc. ',
+          id: 5
+        },
+        {
+          heading: 'WINDOWS:',
+          message1: 'Sliding UPVC windows with designed MS grills.',
+          subheading: 'FINISHING:',
+          message2:
+            'Plastering with cement mortor in double coat sponge and Altek finish.',
+          id: 6
+        },
+        {
+          heading: ' ELECTRICAL:',
+          message1:
+            '3 Phase power with concealed PVC pipes Finolex/Polycab copper wiring, Legrand(Mylinv) modular switches with provision ofAC, Telephone, TV, Fridge, Geyser,Tube lights, Ceiling fan points.',
+          id: 7
+        },
+        {
+          heading: 'WATER SUPPLY:',
+          message1:
+            'Bore well water with submersible moter and RCC overhead tank.',
+          subheading: 'PARKING:',
+          message2: 'One car parking for each flat.',
+          id: 8
+        },
+        {
+          heading: ' PAINTING:',
+          message1:
+            '2 coats interior emulsion over wall care putty for interior walls, enamel paint for wood work, 2 coats of exterior emulsion for exterior surface(Asian/Berger).',
+          id: 9
+        },
+        {
+          heading: 'FLOORING:',
+          message1:
+            '2x2 vetrified tiles flooring in Hall, Bed room, Dining Hall & Kitchen.',
+          subheading: 'PLUMBING:',
+          message2:
+            'CPVC water lines of Astral/Finolex make and jaguar brand mixer sets and Tap fittings.',
+          id: 10
+        },
+        {
+          heading: 'OWNERS COST:',
+          message1:
+            'Registration charges, GST and other taxes levied by Govt. will be at actuals.',
+          id: 11
         }
       ]
     }
